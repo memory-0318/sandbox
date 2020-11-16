6b191fec56d43b84504f22593941c51a46a16e11
Brian Su <brian.su@tpisoftware.com>
2020-11-16 16:07:29 +0800

    Initial commit

---
+++ json-comparison/src/main/java/tw/brian/Application.java
@@ -0,0 +1,16 @@
+package tw.brian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Brian Su <brian.su@tpisoftware.com>
+ * @description:
+ * @date: 2020/11/16
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
