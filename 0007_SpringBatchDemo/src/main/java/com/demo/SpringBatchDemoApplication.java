c228e8057b2c45c03d0b6e22cf4de9163c5b0a60
Brian Su <brian.su@tpisoftware.com>
2022-01-29 09:51:24 +0800

    [0003_SpringBatchDemo] initial commit

---
+++ 0007_SpringBatchDemo/src/main/java/com/demo/SpringBatchDemoApplication.java
@@ -0,0 +1,18 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * @author Brian Su <brian.su@tpisoftware.com>
+ * @description:
+ * @date: 2022/1/28
+ */
+@SpringBootApplication
+public class SpringBatchDemoApplication extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(SpringBootApplication.class);
+        application.run();
+    }
+}
